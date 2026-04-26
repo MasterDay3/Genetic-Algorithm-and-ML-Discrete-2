1d0a1dc4b982d5d1c69e31f9a1629e51748b1921
Zabulskyi Mykola <71492385+MasterDay3@users.noreply.github.com>
2026-04-26 19:06:58 +0300

    add new dataset and fix some bugs

--- baseline.py
+++ baseline.py
@@ -3,7 +3,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 from sklearn.model_selection import cross_val_score
 
-from dataset import X_train, X_test, y_train, y_test, X, y
+from dataset import X_train, X_test, y_train, y_test, X, y, FILENAME
+
 
 def evaluate_baseline(model, X_tr, y_tr, X_te, y_te):
     model.fit(X_tr, y_tr)
@@ -17,16 +18,23 @@ def evaluate_baseline(model, X_tr, y_tr, X_te, y_te):
 
     return acc, f1, roc_auc
 
+
 log_reg = LogisticRegression(max_iter=1000, random_state=42)
 rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
 
-print("=== Baseline Results (Всі ознаки) ===")
+
+print(f"\033[32mStats for {FILENAME}\033[32m")
 
 acc_lr, f1_lr, auc_lr = evaluate_baseline(log_reg, X_train, y_train, X_test, y_test)
-print(f"Logistic Regression -> Accuracy: {acc_lr:.3f}, F1: {f1_lr:.3f}, ROC-AUC: {auc_lr:.3f}")
+print(
+    f"\033[32mLogistic Regression -> Accuracy: {acc_lr:.3f}, F1: {f1_lr:.3f}, ROC-AUC: {auc_lr:.3f}\033[32m"
+)
 
 acc_rf, f1_rf, auc_rf = evaluate_baseline(rf_model, X_train, y_train, X_test, y_test)
-print(f"Random Forest       -> Accuracy: {acc_rf:.3f}, F1: {f1_rf:.3f}, ROC-AUC: {auc_rf:.3f}")
+print(
+    f"\033[32mRandom Forest       -> Accuracy: {acc_rf:.3f}, F1: {f1_rf:.3f}, ROC-AUC: {auc_rf:.3f}\033[32m"
+)
+
 
 def get_fitness_score(selected_features_mask):
     """
@@ -35,6 +43,6 @@ def get_fitness_score(selected_features_mask):
     X_subset = X.iloc[:, selected_features_mask]
 
     model = RandomForestClassifier(n_estimators=50, random_state=42)
-    scores = cross_val_score(model, X_subset, y, cv=3, scoring='f1')
+    scores = cross_val_score(model, X_subset, y, cv=3, scoring="f1")
 
     return scores.mean()
--- dataset.py
+++ dataset.py
@@ -5,20 +5,24 @@ dataset preparing and unpacking
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
-FILENAME = "heart.csv"
+FILENAME = "datasets/heart.csv"
+# FILENAME = "datasets/diabetes.csv"
 
 
 df = pd.read_csv(FILENAME)
+
+target_name = df.columns[-1]
+df = df.drop_duplicates()
 features = df.columns
 feature_types = dict(df.dtypes)
 # targer variable always binary type (0 or 1)
-y = df["target"]
-X = df.drop("target", axis=1)
+y = df[target_name]
+X = df.drop(target_name, axis=1)
 
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=67, stratify=y
 )
 
-print(X_train)
+# print(X_train)
+# print(df)
